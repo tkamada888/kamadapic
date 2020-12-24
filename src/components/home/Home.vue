98fe0f993285544b89eadc09479fe0c4279558ce
Thiago <thiago.kamada@gmail.com>
2020-12-24 16:11:45 +0900

    Including component button

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,80 +1,95 @@
 <template>
   <div>
-
-    <h1 class="centralizado">{{ titulo }}</h1>
-
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-
+    <h1 class="titulo">Alurapic</h1>
+    <input
+      type="search"
+      class="filtro"
+      @input="filtro = $event.target.value"
+      placeholder="filtre pelo título da foto"
+    />
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto.titulo">
+      <li
+        class="lista-fotos-item"
+        v-for="foto in fotosComFiltro"
+        :key="foto.titulo"
+      >
         <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao
+            rotulo="REMOVER"
+            tipo="button"
+            :confirmacao="true"
+            @botaoAtivado="remove(foto)"
+            estilo="perigo"
+          />
         </meu-painel>
       </li>
     </ul>
-
   </div>
 </template>
 
 <script>
-
-import Painel from '../shared/painel/Painel.vue';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Painel from "../shared/painel/Painel.vue";
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
-
   components: {
-
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao,
   },
 
-  data () {
+  data() {
     return {
-      titulo: 'Alurapic', 
+      titulo: "Alurapic",
 
       fotos: [],
 
-      filtro: ''
-    }
+      filtro: "",
+    };
   },
 
   computed: {
     fotosComFiltro() {
       if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter((foto) => exp.test(foto.titulo));
       } else {
         return this.fotos;
       }
-    }
+    },
+  },
+
+  methods: {
+    remove(foto) {
+      alert(`removendo foto de nome ${foto.titulo}`);
+    },
   },
 
   created() {
-
     this.$http
-      .get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos);
-  }
-}
+      .get("http://localhost:3000/v1/fotos")
+      .then((res) => res.json())
+      .then((fotos) => (this.fotos = fotos));
+  },
+};
 </script>
 <style>
+.centralizado {
+  text-align: center;
+}
 
-  .centralizado {
-    text-align: center;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos {
-    list-style: none;
-  }
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
 
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
-  }
+.filtro {
+  display: block;
+  width: 100%;
+}
 </style>
